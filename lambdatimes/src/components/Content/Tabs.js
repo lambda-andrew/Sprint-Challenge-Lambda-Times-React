b827c8a755e7e1d0b543e913695d5003bda22c56
Sydney Crumley <srsimps19@gmail.com>
2019-05-10 11:29:37 -0600

    fixed mapping issues

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -9,7 +9,7 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/
-            props.tabs.map(t => <Tab selectTabHandler={props.selectTabHandler} selectedTab={props.selectedTab} tab={props.tab}/>)}
+            props.tabs.map(t => <Tab selectTabHandler={t.selectTabHandler} selectedTab={t.selectedTab} tab={t.tab}/>)}
       </div>
     </div>
   );
