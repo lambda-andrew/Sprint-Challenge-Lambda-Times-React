2b37343ffd6a41c7dda326f15a14870da3cf8245
atolmie <atolmie22@gmail.com>
2019-05-10 11:09:13 -0500

    filter fixed

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
+import { cardData } from '../../data';
 import Card from './Card';
 
-const Cards = props => {
-  return (
-    <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
-          new Card component for each passing the card as the only prop*/}
-    </div>
-  )
+export default class Cards extends Component {
+  render() {
+    return (
+      <div className="cards-container">
+        {this.props.cards.map((card, idx) =>
+          <Card key={idx} card={card} />
+        )}
+      </div>
+    )
+  }
 }
-
-// Make sure you include prop types for all of your incoming props
-
-export default Cards;
