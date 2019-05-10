6e2c659c11fe8beb77d446ebff32eca55bac77d7
BrennanHolm <brennan.mholmich@gmail.com>
2019-05-10 17:18:06 -0500

    Refactor to use styled components

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,23 +1,95 @@
 import React from 'react';
+import Style from 'styled-components'
 
-// Refactor this component to use styled components and not classNames. 
-// You can find the corresponding CSS in the CSS/index.css file
+const StyleTopBar = Style.div `
+ width: 100%;
+ display: flex;
+ justify-content: center;
+ align-items: none;
+ flex-direction: row;
+ position: fixed;
+ height: 44px;
+ background-color: #333;
+`;
+
+const TBContainer = Style.div `
+ width: 100%;
+ display: flex;
+ justify-content: none;
+ align-items: none;
+ flex-direction: row;
+ color: #fff;
+ letter-spacing: 1px;
+ padding: 0 10px;
+ @media (min-width: 1280px) {
+   width: 1280px;
+ }
+`;
+
+const TBContainerLeft = Style.div `
+ display: flex;
+ justify-content: none;
+ align-items: center;
+ flex-direction: row;
+ flex: 1;
+ font-size: 11px;
+`;
+
+const TBLeftSpan = Style.span `
+ cursor: pointer;
+ margin-right: 25%;
+ font-weight: bold;
+`;
+
+const TBContainerCenter = Style.div `
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ flex-direction: row;
+ flex: 3;
+ font-size: 9px;
+`;
+
+const TBCenterSpan = Style.span `
+ cursor: pointer;
+ margin-right: 5%;
+ :last-child {
+ margin-right: 0
+ }
+ :hover {
+   text-decoration: underline;
+ }
+`;
+
+const TBContainerRight = Style.div `
+ display: flex;
+ justify-content: flex-end;
+ align-items: center;
+ flex-direction: row;
+ flex: 1;
+ font-size: 11px;
+ font-weight: bold;
+`;
+
+const TBRightSpan = Style.span `
+ cursor: pointer;
+`;
 
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
-          <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
-          <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
+    <StyleTopBar>
+      <TBContainer>
+        <TBContainerLeft>
+          <TBLeftSpan>TOPICS</TBLeftSpan><TBLeftSpan>SEARCH</TBLeftSpan>
+        </TBContainerLeft>
+        <TBContainerCenter>
+          <TBCenterSpan>GENERAL</TBCenterSpan><TBCenterSpan>BROWNBAG</TBCenterSpan><TBCenterSpan>RANDOM</TBCenterSpan><TBCenterSpan>MUSIC</TBCenterSpan><TBCenterSpan>ANNOUNCEMENTS</TBCenterSpan>
+        </TBContainerCenter>
+        <TBContainerRight>
+          <TBRightSpan>LOG IN</TBRightSpan>
+        </TBContainerRight>
+      </TBContainer>
+    </StyleTopBar>
   )
 }
 
